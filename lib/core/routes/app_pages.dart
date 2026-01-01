27b7aa336e79c937e6f98a33afd3a0be88e56a96
rufusdev34 <abdul.r@mentorsol.com>
2026-01-01 17:30:49 +0500

    evidence

--- lib/core/routes/app_pages.dart
+++ lib/core/routes/app_pages.dart
@@ -56,6 +56,7 @@ import 'package:alqadiya_game/features/game/controller/cutscene_controller.dart'
 import 'package:alqadiya_game/features/game/controller/question_controller.dart';
 import 'package:alqadiya_game/features/game/controller/user_answer_controller.dart';
 import 'package:alqadiya_game/features/game/controller/suspect_controller.dart';
+import 'package:alqadiya_game/features/game/controller/evidence_controller.dart';
 import 'package:alqadiya_game/features/payment/controller/payment_done_provider.dart';
 import 'package:alqadiya_game/features/change_language/controller/language_controller.dart';
 import 'package:get/get.dart';
@@ -269,6 +270,11 @@ class AppPages {
     GetPage(
       name: AppRoutes.evidenceListScreen,
       page: () => EvidenceListScreen(),
+      bindings: [
+        BindingsBuilder(() {
+          Get.lazyPut(() => EvidenceController());
+        }),
+      ],
       transition: Transition.circularReveal,
       transitionDuration: Duration(milliseconds: 600),
     ),
@@ -277,6 +283,7 @@ class AppPages {
       page: () => ClueDetailScreen(),
       bindings: [
         BindingsBuilder(() {
+          Get.lazyPut(() => EvidenceController());
           Get.lazyPut(() => ClueDetailController());
         }),
       ],
