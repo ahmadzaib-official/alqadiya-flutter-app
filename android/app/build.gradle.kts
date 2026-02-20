3783d8f9e7f839071e5f4b942cde0108fc922a9d
rufusdev34 <abdul.r@mentorsol.com>
2026-02-20 16:33:48 +0500

    Gradle versioning

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -17,11 +17,12 @@ if (keystorePropertiesFile.exists()) {
 android {
     namespace = "com.vga.alqadiya"
     compileSdk = flutter.compileSdkVersion.toInt()
-    ndkVersion = "26.1.10909125"
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_21
         targetCompatibility = JavaVersion.VERSION_21
+        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
@@ -65,4 +66,8 @@ android {
             )
         }
     }
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
