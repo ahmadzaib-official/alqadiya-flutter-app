9fb0c63672f806ef7cbdf201f611cf8aa7e02187
rufusdev34 <abdul.r@mentorsol.com>
2026-02-23 12:17:40 +0500

    keys

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -50,3 +50,15 @@ class MyApp extends StatelessWidget {
 
 // 1234568
 // Qwerty@123
+
+
+// Owner: CN=AhmadZaib, OU=mtrsl, O=MentorSol, L=Lahore, ST=Punjab, C=PK
+// Issuer: CN=AhmadZaib, OU=mtrsl, O=MentorSol, L=Lahore, ST=Punjab, C=PK
+// Serial number: 13ecf3e064e9ee82
+// Valid from: Fri Dec 12 15:35:24 PKT 2025 until: Sun Dec 01 15:35:24 PKT 9991
+// Certificate fingerprints:
+//          SHA1: 2F:B2:73:D1:AD:44:C8:C3:87:23:5E:A7:10:B2:88:65:C4:82:46:68
+//          SHA256: ED:2D:C2:51:79:E6:A6:3B:60:6E:68:ED:8E:EC:C3:53:35:61:DB:B3:42:43:A3:1D:80:B7:A7:80:D0:C7:F7:77
+// Signature algorithm name: SHA384withRSA
+// Subject Public Key Algorithm: 2048-bit RSA key
+// Version: 3
